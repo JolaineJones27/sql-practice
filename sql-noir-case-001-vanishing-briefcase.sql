284db30c07378ad66da4b31431becceed94b5c6d
JolaineJones27 <jolainejones1227@gmail.com>
2025-06-11 07:53:23 -0400

    Update sql-noir-case-001-vanishing-briefcase.sql

--- sql-noir-case-001-vanishing-briefcase.sql
+++ sql-noir-case-001-vanishing-briefcase.sql
@@ -8,6 +8,9 @@ FROM crime_scene
 WHERE location = 'Blue Note Lounge' 
   AND description LIKE '%briefcase%';
 
+-- got this result: id	date	type	location	description
+-- 76	19851120	theft	Blue Note Lounge	A briefcase containing sensitive documents vanished. A witness reported a man in a trench coat with a scar on his left cheek fleeing the scene.
+
 SELECT * 
 FROM suspects
 WHERE description LIKE '%trench coat%';
