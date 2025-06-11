cf9079345515522ce5817484738720b01b6793ed
JolaineJones27 <jolainejones1227@gmail.com>
2025-06-11 02:14:14 -0400

    Update sql-noir-case-002-stolen-sound.sql

--- sql-noir-case-002-stolen-sound.sql
+++ sql-noir-case-002-stolen-sound.sql
@@ -2,3 +2,38 @@
   -- 1. The SQL queries you ran
   -- 2. Any notes or insights as SQL comments
   -- 3. Your final conclusion: who did it?
+
+-- ran this
+SELECT * FROM crime_scene
+WHERE date = 19830715
+  AND location LIKE '%West Hollywood Records%';
+-- got this answer:
+-- Results
+-- id	date	type	location	description
+-- 65	19830715	theft	West Hollywood Records	A prized vinyl record was stolen from the store during a busy evening.
+
+  -- ran this
+  SELECT clue FROM witnesses
+WHERE crime_scene_id = (
+    SELECT id FROM crime_scene
+    WHERE date = 19830715
+      AND location LIKE '%West Hollywood Records%'
+);
+--got this answer: 
+-- Results
+-- clue
+-- I saw a man wearing a red bandana rushing out of the store.
+-- The main thing I remember is that he had a distinctive gold watch on his wrist.
+
+-- ran this 
+WHERE bandana_color IN (
+    SELECT clue FROM witnesses
+    WHERE crime_scene_id = (
+        SELECT id FROM crime_scene
+        WHERE date = 19830715
+          AND location LIKE '%West Hollywood Records%'
+    )
+)
+-- got no result? Results
+-- No results yet. Execute a query to see the results.
+
