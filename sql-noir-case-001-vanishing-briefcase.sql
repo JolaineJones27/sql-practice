b7b358b6c086d7679a810e0f2df52616a1d3e3b1
JolaineJones27 <jolainejones1227@gmail.com>
2025-06-11 01:57:34 -0400

    Update sql-noir-case-001-vanishing-briefcase.sql

--- sql-noir-case-001-vanishing-briefcase.sql
+++ sql-noir-case-001-vanishing-briefcase.sql
@@ -2,3 +2,26 @@
   -- 1. The SQL queries you ran
   -- 2. Any notes or insights as SQL comments
   -- 3. Your final conclusion: who did it?
+
+SELECT *
+FROM crime_scene
+WHERE location = 'Blue Note Lounge' 
+  AND description LIKE '%briefcase%';
+
+SELECT * 
+FROM suspects
+WHERE description LIKE '%trench coat%';
+
+SELECT transcript FROM interviews
+WHERE suspect_id = (
+    SELECT id FROM suspects
+    WHERE attire LIKE '%trench coat%'
+);
+
+-- ID 76 did it!
+
+
+-- ok, the SELECT * FROM suspects WHERE 'description' LIKE '%trench coat%' works and I don't know why but it didn't at first without the '' 
+
+-- I'm kinda confused. only one result came up I think - is it the same answer?
+
